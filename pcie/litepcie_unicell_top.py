4f5fece74c9c652e1e75d9671967d308f63e15eb
Alan <alh@imago-unicell.dev>
2026-05-23 17:37:54 +0000

    pcie: remove comma from ident string (LiteX rejects commas)

--- pcie/litepcie_unicell_top.py
+++ pcie/litepcie_unicell_top.py
@@ -146,7 +146,7 @@ class LitePCIeUniCellSoC(SoCMini):
         sys_clk_freq  = int(200e6)
 
         SoCMini.__init__(self, platform, sys_clk_freq,
-            ident     = f"Imago-UniCell PCIe Accelerator (x{nlanes} Gen2, {NUM_CELLS} cells)",
+            ident     = f"Imago-UniCell PCIe x{nlanes} Gen2 {NUM_CELLS} cells",
             with_ctrl = False,
         )
 
